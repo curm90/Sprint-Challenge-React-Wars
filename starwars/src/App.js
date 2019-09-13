3f1374866e46be0ab73d8918cc8d01fab40138e2
Liam Sutton <devliam90@gmail.com>
2019-09-13 08:52:22 +0100

    add pagination

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -5,20 +5,40 @@ import { CharacterList } from './components/CharacterList';
 
 const App = () => {
   const [characters, setCharacters] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
 
   useEffect(() => {
     axios
-      .get(`https://swapi.co/api/people/`)
+      .get(`https://swapi.co/api/people/?page=${currentPage}`)
       .then(res => {
         setCharacters(res.data.results);
       })
       .catch(err => console.log(err));
-  }, []);
+  }, [currentPage]);
+
+  const nextButton = () => {
+    if (currentPage < 9) {
+      setCurrentPage(currentPage + 1);
+    }
+  };
+
+  const prevButton = () => {
+    if (currentPage === 1) return;
+    return setCurrentPage(currentPage - 1);
+  };
 
   return (
     <div className="App">
       <h1 className="Header">React Wars</h1>
       <CharacterList data={characters} />
+      <div className="button-container">
+        <button onClick={() => prevButton()} className="prevBtn">
+          prev
+        </button>
+        <button onClick={() => nextButton()} className="nextBtn">
+          next
+        </button>
+      </div>
     </div>
   );
 };
