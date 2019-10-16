e87e848e40f547ae5dc994be375c4de512236b16
Liam Sutton <devliam90@gmail.com>
2019-10-16 19:53:30 +0100

    style tweaks

--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -8,7 +8,7 @@ const CharacterCard = styled.div`
   margin: 1rem 0;
   background: #dbdac9;
   border-radius: 10px;
-  box-shadow: 2px 2px 3px black;
+  box-shadow: 3px 4px 12px black;
   transition: ease-in-out 0.12s;
 
   &:hover {
@@ -27,9 +27,23 @@ const CharacterCard = styled.div`
     padding-bottom: 1rem;
   }
 
-  p {
-    font-size: 1.3rem;
-    color: #232322;
+  .info {
+    display: flex;
+    flex-direction: column;
+    /* justify-content: space-between;
+    align-items: center; */
+    padding: 0 2rem;
+
+    p {
+      font-size: 1.3rem;
+      color: #232322;
+      width: 100%;
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      border-bottom: 1px solid black;
+      padding-bottom: 1rem;
+    }
   }
 
   .icon {
@@ -42,28 +56,32 @@ export const Character = ({ data }) => {
   return (
     <CharacterCard>
       <h2>{data.name}</h2>
-      <p>
-        <span>Hair Color: </span>
-        {data.hair_color}
-      </p>
-      <p>
-        <span>Eye Color: {data.eye_color}</span>
-      </p>
-      <p>
-        <span>Gender: {data.gender}</span>
-      </p>
-      <p>
-        <span>Weight: </span>
-        {data.mass} Kg
-      </p>
-      <p>
-        <span>Height: </span>
-        {data.height} cm
-      </p>
-      <p>
-        <span>DOB: </span>
-        {data.birth_year}
-      </p>
+      <div className="info">
+        <p>
+          <span>Hair Color: </span>
+          {data.hair_color}
+        </p>
+        <p>
+          <span>Eye Color: </span>
+          {data.eye_color}
+        </p>
+        <p>
+          <span>Gender: </span>
+          {data.gender}
+        </p>
+        <p>
+          <span>Weight: </span>
+          {data.mass} Kg
+        </p>
+        <p>
+          <span>Height: </span>
+          {data.height} cm
+        </p>
+        <p>
+          <span>DOB: </span>
+          {data.birth_year}
+        </p>
+      </div>
       <img className="icon" src={logo} alt="lightsabers"></img>
     </CharacterCard>
   );
